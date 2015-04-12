f188dbe30ef2fb1c215db53c61b2b029da5e5794
Olivier El Mekki <olivier@el-mekki.com>
2015-04-12 22:17:56 +0200

    ADD Etag support
    In order to take advantage of product hunt's API Etag support, allows to
    retrieve etag and to set custom headers for requests.
    
    ```ruby
    client = ProductHunt::Client.new('mytoken')
    post = client.post(3372)
    etag = client.last_etag # retrieve etag from last request
    
    post = client.post(3372, headers: { 'If-None-Match': etag }) # only newer records
    ```
    
    If there is no new record (response will return a 304 status), API
    methods will just return nil.

--- lib/product_hunt/api/posts.rb
+++ lib/product_hunt/api/posts.rb
@@ -5,20 +5,23 @@ module ProductHunt
       PATH = "/posts"
 
       def posts(options = {})
-        fetch(PATH, options)["posts"].map{ |post| Post.new(post, self) }
+        response = fetch(PATH, options)
+        response["posts"].map{ |post| Post.new(post, self) } if response.code == 200
       end
 
       def post(id, options = {})
-        post = fetch(PATH + "/#{id}", options)["post"]
-        Post.new(post, self)
+        post = fetch(PATH + "/#{id}", options)
+        Post.new(post["post"], self) if post.code == 200
       end
 
       def comments_for_post(id, options = {})
-        fetch(PATH + "/#{id}/comments", options)["comments"].map{ |c| Comment.new(c, self) }
+        response = fetch(PATH + "/#{id}/comments", options)
+        response["comments"].map{ |c| Comment.new(c, self) } if response.code == 200
       end
 
       def votes_for_post(id, options = {})
-        fetch(PATH + "/#{id}/votes", options)["votes"].map{ |c| Vote.new(c, self) }
+        response = fetch(PATH + "/#{id}/votes", options)
+        response["votes"].map{ |c| Vote.new(c, self) } if response.code == 200
       end
     end
   end
