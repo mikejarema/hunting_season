f739267799a1d40ee31f4c70433aaab37a99dea9
Stuart Chaney <stuartchaney22@gmail.com>
2015-05-15 13:29:48 -0700

    support for users endpoint

--- lib/product_hunt/api/users.rb
+++ lib/product_hunt/api/users.rb
@@ -4,6 +4,10 @@ module ProductHunt
 
       PATH = "/users"
 
+      def users(options = {})
+        fetch(PATH, options)["users"].map{ |user| User.new(user, self) }
+      end
+
       def user(id, options = {})
         user = fetch(PATH + "/#{id}", options)["user"]
         User.new(user, self)
